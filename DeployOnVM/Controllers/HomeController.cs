c0de1fad91d6ec383d4a23323b6cea43500a9d2c
Lalit Diwate <lalit.diwate@perficient.com>
2023-11-28 14:46:52 +0530

    Add project files.

---
+++ DeployOnVM/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using DeployOnVM.Models;
+using DeployOnVM.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeployOnVM.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public List<Product> Products;
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			ProductService productService = new ProductService();
+			Products = productService.GetProducts();
+			return View(Products);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ DeployOnVM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeployOnVM.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
